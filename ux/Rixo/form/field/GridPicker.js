84e612defa498697d13b89a855bd6ca8b9036532
Éric Ortega <eric@eoko.fr>
2014-02-12 18:41:01 +0100

    Cleaned out incorrect license information.

--- ux/Rixo/form/field/GridPicker.js
+++ ux/Rixo/form/field/GridPicker.js
@@ -1,29 +1,6 @@
 /**
- * Copyright (C) 2013
- *
- * This file is part of Opence.
- *
- * Opence is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Opence is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Opence. If not, see <http://www.gnu.org/licenses/gpl.txt>.
- *
- * @copyright Copyright (C) 2013 Eoko
- * @licence http://www.gnu.org/licenses/gpl.txt GPLv3
- * @author Éric Ortega <eric@eoko.fr>
- */
-
-/**
- *
  * @since 2013-06-19 21:55
+ * @author Éric Ortega <eric@planysphere.fr>
  */
 Ext.define('Ext.ux.Rixo.form.field.GridPicker', {
 	extend: 'Ext.form.field.ComboBox'
